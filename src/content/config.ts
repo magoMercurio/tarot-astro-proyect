9b7532c720537d9ac97b7683a2e00f2bb3842599
mago_d_Mercurio <davila.feria@gmail.com>
2024-10-22 15:21:27 -0500

    add content tarot keys

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,25 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const tarotkeys = defineCollection({
+	type: 'content',
+	schema: z.object({
+		nombre: z.string(),
+		descripcion: z.string(),
+		numero: z.number(),
+		letra: z.string(),
+		nombreLetra: z.string(),
+		significadoLetra: z.string(),
+		color: z.string(),
+		notaMusical: z.string(),
+		astrologica: z.string(),
+		inteligencia: z.string(),
+		alquimica: z.string(),
+		elemento: z.string(),
+		palabraClave: z.string(),
+		otras: z.string(),
+		tarotImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, tarotkeys };
